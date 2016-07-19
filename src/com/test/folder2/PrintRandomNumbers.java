b338d984608f83858c5d6cb61b4c9cbefcffcd20
rparmar2 <rparmar@coach.com>
2016-07-19 13:42:17 -0400

    Committing

--- src/com/test/folder2/PrintRandomNumbers.java
+++ src/com/test/folder2/PrintRandomNumbers.java
@@ -3,8 +3,12 @@ package com.test.folder2;
 public class PrintRandomNumbers {
 	
 	public static void main (String[]args){
-		double i = Math.random();
-		System.out.println("I generated a random number "+ i);
+		double j = Math.random();
+		System.out.println("I generated a random number "+ j);
+		
+		for(int i =0 ;i < 2 ; i++){
+			System.out.println("testing "+ i);
+		}
 	}
 
 }
